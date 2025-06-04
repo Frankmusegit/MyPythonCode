9f5d24eddd9f88b4f19ac9f20f5c76dc11160302
Frankmusegit <42666695+Frankmusegit@users.noreply.github.com>
2025-06-04 11:03:03 +0200

    Delete tmp directory

--- tmp/BSymbol.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-#-*- coding:utf-8 -*-
-from Element import *
-
-class BSymbol(Element):
-    def __init__(self, s):
-        Element.__init__(self, s)
--- tmp/Ottava.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-#-*- coding:utf-8 -*-
-from Textline import *
-
-class Ottava(TextLine):
-    def __init__(self, s):
-        TextLine.__init__(self, s)
-        self.setSubtype(0)
